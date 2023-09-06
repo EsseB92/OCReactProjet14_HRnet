03dd60450e696db306bd55cc3f4a36ae69c5e8d2
Bertrand Sudre <sudrebertrand@gmail.com>
2023-09-06 16:42:12 +0200

    Création de plusieurs employés

---
+++ src/features/employees/employeesItems.js
@@ -0,0 +1,28 @@
+const employeesItems = [
+  {
+    id: 1,
+    firstName: "aaaa",
+    lastName: "bbbb",
+    birthDate: "31/12/2000",
+    street: "cccc",
+    city: "dddd",
+    state: "Alaska",
+    zipCode: "75001",
+    startDate: "25/02/2020",
+    department: "Sales",
+  },
+  {
+    id: 2,
+    firstName: "aaaa",
+    lastName: "bbbb",
+    birthDate: "31/12/2000",
+    street: "cccc",
+    city: "dddd",
+    state: "Alaska",
+    zipCode: "75001",
+    startDate: "25/02/2020",
+    department: "Sales",
+  },
+];
+
+export default employeesItems;
