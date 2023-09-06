b9e839ceac761a517f23f2fa0ccebe2a3da55cbe
Bertrand Sudre <sudrebertrand@gmail.com>
2023-09-06 16:44:38 +0200

    Création de la page EmployeeList

--- src/pages/EmployeeList.jsx/index.jsx
+++ src/pages/EmployeeList.jsx/index.jsx
@@ -1,9 +1,124 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import Table from "../../components/Table";
+import styles from "./index.module.css";
+import Search from "../../assets/search.svg";
+import { deleteEmployees } from "../../features/employees/employeesSlice";
+import { Link } from "react-router-dom";
+
+const columns = [
+  {
+    name: "First Name",
+    selector: (row) => row.firstName,
+    sortable: true,
+  },
+  {
+    name: "Last Name",
+    selector: (row) => row.lastName,
+    sortable: true,
+  },
+  {
+    name: "Date of Birth",
+    selector: (row) => row.birthDate,
+    sortable: true,
+  },
+  {
+    name: "Street",
+    selector: (row) => row.street,
+    sortable: true,
+  },
+  {
+    name: "City",
+    selector: (row) => row.city,
+    sortable: true,
+  },
+  {
+    name: "State",
+    selector: (row) => row.state,
+    sortable: true,
+  },
+  {
+    name: "Zip Code",
+    selector: (row) => row.zipCode,
+    sortable: true,
+  },
+  {
+    name: "Start Date",
+    selector: (row) => row.startDate,
+    sortable: true,
+  },
+  {
+    name: "Department",
+    selector: (row) => row.department,
+    sortable: true,
+  },
+];
 
 const EmployeeList = () => {
+  const employees = useSelector((state) => state.employees);
+  const data = employees.employees;
+  const dispatch = useDispatch();
+
+  const [records, setRecords] = useState(data);
+  const [selectedRows, setSelectedRows] = useState([]);
+
+  const handleFilter = (event) => {
+    event.preventDefault();
+
+    const searchText = event.target.value.trim().toLowerCase();
+
+    const newData = data.filter((row) => {
+      const employeeDataString = Object.values(row).join(" ").toLowerCase();
+      return employeeDataString.includes(searchText);
+    });
+
+    setRecords(newData);
+  };
+
+  const handleDelete = (event) => {
+    event.preventDefault();
+    dispatch(deleteEmployees(selectedRows.map((employee) => employee.id)));
+  };
+
+  useEffect(() => {
+    setRecords(data);
+  }, [data]);
+
+  useEffect(() => {
+    setRecords(data);
+  }, [data]);
+
   return (
-    <div>
-      <h1>Current Employees</h1>
+    <div className={styles.container}>
+      <h1 className={styles.title}>Current Employees</h1>
+      <div style={{ position: "relative" }}>
+        <div className={styles.search}>
+          <img className={styles.icon} src={Search} alt="" />
+          <input
+            className={styles.input}
+            type="text"
+            onChange={handleFilter}
+            placeholder="Filter"
+          />
+        </div>
+
+        <Table
+          columns={columns}
+          data={records}
+          setSelectedRows={setSelectedRows}
+        ></Table>
+        {records && selectedRows.length > 0 && (
+          <button
+            onClick={handleDelete}
+            style={{ position: "absolute", bottom: "20px" }}
+          >
+            Delete
+          </button>
+        )}
+      </div>
+      <Link to="/" relative="path" className={styles.link}>
+        Create an employee
+      </Link>
     </div>
   );
 };
