04a4b4fb1652c416b2f6965161757ce697a640c2
Bertrand Sudre <sudrebertrand@gmail.com>
2023-09-06 16:36:31 +0200

    Mise en place du store persistant (redux et redux-persist)

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import { store, persistor } from "./app/store.js";
+
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-    <App />
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
+    </Provider>
   </BrowserRouter>
 );
