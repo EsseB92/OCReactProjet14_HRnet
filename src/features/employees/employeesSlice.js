75d0b8f89f2f54031cee8e44c16d7b878f82536b
Bertrand Sudre <sudrebertrand@gmail.com>
2023-09-07 18:31:43 +0200

    Ajout d'un selector et de la documentation

--- src/features/employees/employeesSlice.js
+++ src/features/employees/employeesSlice.js
@@ -1,20 +1,42 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSelector, createSlice } from "@reduxjs/toolkit";
 import employeesItems from "./employeesItems";
 
+/**
+ * Initialise l'état par défaut.
+ */
 const initialState = {
   employees: employeesItems,
 };
 
+/**
+ * Slice pour gérer les employés.
+ *
+ * @type {import('@reduxjs/toolkit').Slice}
+ */
 const employeesSlice = createSlice({
   name: "employees",
   initialState,
   reducers: {
+    /**
+     * Ajoute un nouvel employé à la liste des employés.
+     *
+     * @param {Object} state - État actuel.
+     * @param {Object} action - Action contenant le nouvel employé.
+     * @returns {Object} Le nouvel état
+     */
     addEmployee: (state, action) => {
       return {
         ...state,
         employees: [...state.employees, action.payload],
       };
     },
+    /**
+     * Supprime les employés spécifiés de la liste des employés.
+     *
+     * @param {Object} state - État actuel.
+     * @param {Object} action - Action contenant les IDs des employés à supprimer.
+     * @return {Object} Le nouvel état
+     */
     deleteEmployees: (state, action) => {
       const employeesToDelete = action.payload;
       const newEmployees = state.employees.filter(
@@ -29,4 +51,16 @@ const employeesSlice = createSlice({
 });
 
 export const { addEmployee, deleteEmployees } = employeesSlice.actions;
+
+export const selectAllEmployees = (state) => state.employees.items;
+/**
+ * Selector pour obtenir la liste de tous les employés.
+ *
+ * @type {import('@reduxjs/toolkit').Selector}
+ */
+export const selectAllEmployeesSelector = createSelector(
+  [selectAllEmployees],
+  (employees) => employees
+);
+
 export default employeesSlice.reducer;
