b5356018717d5dfd7cec368dd23691dcb3e7dded
Bertrand Sudre <sudrebertrand@gmail.com>
2023-09-10 19:22:53 +0200

    Ajout du container de toastify

--- src/main.jsx
+++ src/main.jsx
@@ -5,14 +5,17 @@ import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 import { store, persistor } from "./app/store.js";
+import "react-toastify/dist/ReactToastify.css";
 
 import "./index.css";
+import { ToastContainer } from "react-toastify";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
         <App />
+        <ToastContainer />
       </PersistGate>
     </Provider>
   </BrowserRouter>
