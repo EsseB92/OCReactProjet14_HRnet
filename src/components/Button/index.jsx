61950a0b572c9896bccee0fe1e261141855234cb
Bertrand Sudre <sudrebertrand@gmail.com>
2023-09-10 19:23:26 +0200

    MAJ du composant Button

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,3 +1,11 @@
-export const Button = ({ children }) => {
-  return <button>Je suis un bouton {children}</button>;
+import Styles from "./index.module.css";
+
+export const Button = ({ className, onClick, children }) => {
+  return (
+    <button className={`${Styles.button} ${className}`} onClick={onClick}>
+      {children}
+    </button>
+  );
 };
+
+export default Button;
